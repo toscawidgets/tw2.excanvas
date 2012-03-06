fbdb6383e783b508c8ebb4d6c0035982c1ede884
Ralph Bean <rbean@redhat.com>
2012-03-06 10:18:45 -0500

    Changed email address.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='toscawidgets2 wrapper for excanvas.js resource',
     long_description='',
     author='Ralph Bean',
-    author_email='ralph.bean@gmail.com',
+    author_email='rbean@redhat.com',
     license='MIT',
     url='http://github.com/toscawidgets/tw2.excanvas',
     install_requires=[
